c303ade4085c67d2bdd490fc9baa909d008fe0b4
Mike <mike@epicodus.com>
2016-05-02 11:39:29 -0700

    add initial files

---
+++ src/test/java/TaskTest.java
@@ -0,0 +1,26 @@
+import org.sql2o.*;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class TaskTest {
+
+  @Before
+  public void setUp() {
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", null, null);
+  }
+
+  @After
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "DELETE FROM tasks *;";
+      con.createQuery(sql).executeUpdate();
+    }
+  }
+
+  @Test
+  public void Task_instantiatesCorrectly() {
+    Task testTask = new Task();
+    assertEquals(true, testTask instanceof Task);
+  }
+
+}
