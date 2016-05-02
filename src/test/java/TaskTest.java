52a5c58323f132369c667dc235c6c7796cdd4385
Mike <mike@epicodus.com>
2016-05-02 11:43:16 -0700

    add tasks db support (follow along to do list with sql2o and postgres lesson)

--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -18,9 +18,50 @@ public class TaskTest {
   }
 
   @Test
-  public void Task_instantiatesCorrectly() {
-    Task testTask = new Task();
-    assertEquals(true, testTask instanceof Task);
+  public void Task_instantiatesCorrectly_true() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals(true, myTask instanceof Task);
+  }
+
+  @Test
+  public void getDescription_taskInstantiatesWithDescription_String() {
+    Task myTask = new Task("Mow the lawn");
+    assertEquals("Mow the lawn", myTask.getDescription());
+  }
+
+  @Test
+  public void all_emptyAtFirst() {
+    assertEquals(Task.all().size(), 0);
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescriptionsAretheSame() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Mow the lawn");
+    assertTrue(firstTask.equals(secondTask));
+  }
+
+  @Test
+  public void save_returnsTrueIfDescriptionsAretheSame() {
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    assertTrue(Task.all().get(0).equals(myTask));
+  }
+
+  @Test
+  public void save_assignsIdToObject() {
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    Task savedTask = Task.all().get(0);
+    assertEquals(myTask.getId(), savedTask.getId());
+  }
+
+  @Test
+  public void find_findsTaskInDatabase_true() {
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    Task savedTask = Task.find(myTask.getId());
+    assertTrue(myTask.equals(savedTask));
   }
 
 }
